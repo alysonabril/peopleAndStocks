9a5a8bc91f41ceca348fcee4033cbc903fcd3fa2
alysonabril <alysonabril@pursuit.org>
2019-09-05 13:33:52 -0400

    set up people model

--- peopleAndStocks/Model/People.swift
+++ peopleAndStocks/Model/People.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+struct People: Codable {
+    let results: [PersonInfo]
+}
+
+struct PersonInfo: Codable {
+    let name: Name
+    let location: Location
+    let email: String
+    let phone: String
+    let dob: String
+    let picture: Picture
+}
+
+struct Name: Codable {
+    let first: String
+    let last: String
+    
+    public var fullName: String {
+        return "\(first.capitalized) \(last.capitalized)"
+    }
+}
+
+struct Location: Codable {
+    let street: String
+    let city: String
+    let state: String
+    let postcode: String
+    
+    public var address: String {
+        return "\(street) \n\(city.capitalized), \(state.capitalized) \(postcode)"
+    }
+}
+
+struct Picture: Codable {
+    let large: URL
+    let medium: URL
+    let thumbnail: URL
+}
